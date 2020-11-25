ca8ef91d8149621f9629359044fd8b3a47d0a999
Alex S <55304780+SolerJapan@users.noreply.github.com>
2020-11-25 08:29:35 -0500

    Add files via upload

---
+++ week2day2mrnlab.js
@@ -0,0 +1,68 @@
+// var num = 1;
+// while(num<=20)
+// {
+//   if(num%2==0)
+//   {
+//     console.log(num*3);
+//   }
+//   num++;
+// }
+
+//exercise
+
+// var bill = 6.00;
+// var quarter = .25;
+// var count=0;
+// while (bill!=0)
+// {
+//    bill=bill-quarter;
+//    count++;
+// }
+// console.log(count + " is the quarters returned");
+
+//lab work
+
+// var prime = false;
+// let num =1;
+// while(num<=20)
+// {
+//   let pCount= 2;
+//   while(pCount<num)
+//   {
+//     if(num%pCount==0)
+//     {
+//       prime = false;
+//       break;
+//     }
+//     else
+//     {
+//       prime = true;
+//     }
+//     pCount++
+//   }
+//   if(prime == true)
+//   {
+//      console.log(num + " is prime");
+//   }
+//   num++;
+// }
+
+// let sum = 0;
+// let num = 1;
+// while(num<=20)
+// {
+//    sum+=num;
+//    num++;
+// }
+// console.log(sum + " is the sum");
+
+
+let num = 1;
+while(num<=20)
+{
+   if(num%2==0)
+   {
+     console.log(num + " is even");
+   }
+   num++;
+}
